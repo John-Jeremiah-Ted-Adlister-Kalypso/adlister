c1bf3bcdf10fd74aeae444674b2ecaa9c1442242
Ted McCormick <ted.mccormick@gmail.com>
2021-03-16 14:14:22 -0500

    Functioning with a Java loops

--- src/main/java/com/codeup/adlister/controllers/DetailsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DetailsServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +9,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
 
 @WebServlet(name = "controllers.DetailsServlet", urlPatterns = "/details")
-public class AdsIndexServlet extends HttpServlet {
+public class DetailsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int id = request.getParameter("id");
+        int id = parseInt(request.getParameter("id"));
+        Ad displayAd = null;
+//        Ad testAd = new Ad(1, 1, "Look at this great ad", "It's right here for you.");
+        List<Ad> adList = DaoFactory.getAdsDao().all();
+        for (Ad ad : adList) {
+            if (ad.getId() == id) {
+                displayAd = ad;
+            }
+        }
+        if (displayAd == null) {
+            response.getWriter().println("<h1>Hey, sorry, something went wrong</h1>");
+        } else {
+            request.setAttribute("displayAd", displayAd);
 
-        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/ads/details.jsp").forward(request, response);
+        }
     }
 }
